0f6742689ad9e3738dc4070eb55ca72c02b8e243
Raghu Betina <raghu@firstdraft.com>
2018-04-10 04:54:42 +0000

    Direct association between dishes and bucket_lists

--- app/models/bucket_list.rb
+++ app/models/bucket_list.rb
@@ -1,6 +1,9 @@
 class BucketList < ApplicationRecord
   # Direct associations
 
+  belongs_to :dishes,
+             :class_name => "Dish"
+
   belongs_to :users,
              :class_name => "User"
 
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,10 @@
 class Dish < ApplicationRecord
   # Direct associations
 
+  has_many   :bucket_lists,
+             :foreign_key => "dishes_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
