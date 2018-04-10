42eb3d48e646986d5ba778e512db4d79ebad2a5d
Raghu Betina <raghu@firstdraft.com>
2018-04-10 04:55:01 +0000

    Whitelist columns in BucketList for ActiveAdmin

--- app/admin/bucket_lists.rb
+++ app/admin/bucket_lists.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register BucketList do
+
+ permit_params :bucketlist, :users_id, :dishes_id, :selectedvenue_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
